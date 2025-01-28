b2b5bf7a9ef3242e664960590e324ceb464bf382
Gabriel Kubota <gabrielkhiroshi@gmail.com>
2025-01-28 13:22:53 -0300

    tests

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from './node_modules/@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'test-app',
-  webDir: '.next',
+  webDir: 'out',
   server: {
     url: "http://192.168.1.119:3000",
     cleartext: true,
